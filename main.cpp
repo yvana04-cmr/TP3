e015c8b9107b80a46e8cf490da8bdd6987001d5f
yvana04-cmr <toukoyvana@gmail.com>
2023-01-17 13:47:36 +0100

    Classe main ajoutée

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "Magasin.h"
+//#include "Date.h"
+
+int main (){
+ Produit p ("Air Force1", "basket", 5, 150.00);
+Produit p1 ("iPhone X", "Télephone Apple 256Go", 1, 350.00);
+Produit p2 ("Air Jordan", "basket", 2, 100.00);
+Produit p3 ("HP Pavillon", "ordinateur portable", 1, 500.00);
+
+ std::cout << p;
+    Magasin m;
+    m.addProduit(p);
+    m.addProduit(p1);
+    m.addProduit(p2);
+    m.affichageProduit();
+
+/*Date d1(4,5,2004);
+Date d6(5,5,2004);
+Date d2(31,12,2021);
+Date d3(1,1,2021);
+Date d4(25,6,2003);
+Date d5(25,7,2003);
+
+//d1.updateMonth(6);
+
+//d2.next();
+
+std::cout << d1;
+std::cout << d2;
+std::cout << d3;
+std::cout << d4;
+std::cout << d5;
+std::cout << d6; 
+
+d1.addDate(d1);
+d2.addDate(d2);
+d3.addDate(d3);
+d4.addDate(d4);
+d5.addDate(d5);
+d6.addDate(d6);
+
+d1.quickselect(d1);
+d2.quickselect(d2);
+d3.quickselect(d3);
+d4.quickselect(d4);
+d5.quickselect(d5);*/
+
+     return 0;
+}
